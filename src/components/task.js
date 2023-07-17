b7337cb5684b008a280ba1d42409d936be3f0ac3
AhooraGorji <99167642+ahouragorji@users.noreply.github.com>
2023-07-17 21:08:20 +0330

    Added editing ! also did some styling change!

---
+++ src/components/task.js
@@ -0,0 +1,50 @@
+import React, { useRef, useState,useEffect } from "react";
+import "./task.css"
+export default function Task(props){
+    const [name,setName] = useState(props.name)
+    const[id] = useState(props.id)
+    const[status,setStatus]=useState(props.status)
+    const[newName,setNewName] =useState("")
+    const handleDelete = props.handleDelete;
+    const refEdit = useRef(null)
+    
+    useEffect(() => {
+        if (status === "edit") {
+          refEdit.current.focus();
+        }
+      }, [status]);
+
+    const handleEdit = (e)=>{
+
+        setNewName(e.target.value)
+    }
+    const handleSubmit=(e)=>{
+        setName(newName)
+        setStatus('done');
+    }
+    if(status==='edit'){
+        return(
+    <div class='container'>
+    <div class='inputContainer'><input type='text' ref={refEdit}  value={newName} onChange={handleEdit}/>
+    </div>
+    <div class = 'buttons container'>
+    <button id ='submitButton' onClick={()=> handleSubmit(id)}>✔️</button>
+    <button id='deleteButton' onClick={()=> handleDelete(id)}>❌</button>
+    </div>
+    </div>
+        )
+    }
+
+    if(status==='done'){
+return(
+    <div class='container'>
+    <div id='name'>{name}</div>
+    <div class = 'buttons container'>
+    <button id ='ediButton' onClick={()=> {setNewName(name); setStatus('edit')} }>🖊</button>
+    <button id='deleteButton' onClick={()=> handleDelete(id)}>❌</button>
+    </div>
+    </div>
+    )
+    }
+    
+}
--- src/components/todo-element.js
+++ src/components/todo-element.js
@@ -1,36 +1,42 @@
 import React, { useState } from "react";
 import './todo.css'
+import Task from "./task";
 export default function TodoElement(){
     const [list,setList] = useState([]);
     const[newItem,setNewItem] = useState("");
+    const [addButtonActive,setAddButtonActive] = useState("hidden");
 
     const handleClick =()=>{
         const newListItem = {id:Math.random()*1000,name:newItem}
         setList([...list, newListItem])
         setNewItem("")
+        setAddButtonActive("hidden")
     }
     const handleType =(e)=>{
-        setNewItem(e.target.value)
+      setAddButtonActive("visible")
+      setNewItem(e.target.value)
     }
     const handleDelete =(id)=>{
         const newList = list.filter(item => item.id!==id)
         console.log('deleted')
         setList(newList)
     }
+    
   return(
     <div>
     {/*header*/}
     <header>Todo!</header>
     {/*input */}
-    <input type = 'text' placeholder='Add Something' value={newItem}onChange={handleType}/>
-    <button onClick={handleClick}>+</button>
+    <div className="header-container">
+    <input type = 'text' placeholder='Add Something' value={newItem} onChange={handleType}/>
+    <button style={{visibility:{addButtonActive}}} onClick={handleClick}>➕</button>
+    </div>
     {/*list*/}
     <ul className="list-unstyled">
     {
         list.map((item)=>(
         <li key={item.id}>
-        <div className="item-name">{item.name}</div>
-        <button onClick={()=> handleDelete(item.id)}>❌</button>
+        <div className="item-name"><Task name={item.name} id={item.id} status="done" handleDelete={handleDelete} /></div>
         </li>
                         )
                                 )
